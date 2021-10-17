04e1d9cfb3575d0a7139665865807bef2ef77c4d
amisha0403 <singhamisa11@gmail.com>
2021-10-17 23:58:00 +0530

    route file pushed

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       devise_for :users, controllers: { sessions: 'api/v1/sessions', registrations: 'api/v1/registrations' } 
-      resources :courses
       resources :answers
+      resources :courses
+      resources :exams
       resources :questions
+      resources :results
     end
   end
 end
